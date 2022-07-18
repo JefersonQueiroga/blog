3ea0022eed3ed7c896e2cae3b75a69cf5755b683
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2022-07-18 15:49:03 -0300

    Adicionar dados

--- postagem/models.py
+++ postagem/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    nome_curso = models.CharField(max_length=140)
+    data_criacao = models.DateField()
+    nome_coordenador = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_curso
+
+
+class Aluno(models.Model):
+    nome_aluno = models.CharField(max_length=140)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    curso = models.ForeignKey(Curso,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome_aluno
